7021f237fa8386023810eab390e2b6a43869f823
Pablo Rosas <pablorosas3115@gmail.com>
2022-12-02 22:34:04 -0600

    firt commit

---
+++ src/main/java/com/pablorosas/app/App.java
@@ -0,0 +1,13 @@
+package com.pablorosas.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/controller/com/miclase.java
@@ -0,0 +1,15 @@
+package controller.com;
+
+public class miclase {
+
+	private String nombre = "Pablo";
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
